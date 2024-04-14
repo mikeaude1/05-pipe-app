b5826cef6374f474fb7f04436c98402987ad0d85
mikeaude1 <miguelaude@ymail.com>
2024-04-13 23:36:38 -0600

    nuevo comit

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval, tap } from 'rxjs';
 
 @Component({
   selector: 'app-uncommon-page',
@@ -21,6 +22,24 @@ public clientsMap={
   '=2':'tenemos 2 personas esperando.',
   'other':'tenemos # clientes esperando.'
 }
+//key pipe and json pipe
+public person= {
+  name:'Miguel',
+  age:'39',
+  adderess:'Chihuahua, México'
+}
+
+//Async Pipe
+public myObserbaleTimer = interval(2000).pipe(
+  tap(value=>console.log('tap:',value))
+)
+public pomiseValue: Promise<string> = new Promise((resolve,reject)=>{
+  setTimeout(()=>{
+    resolve('Tenemos data en la promesa.');
+    console.log('Tenemos data en la promesa.');
+    this.person.name= 'Otro Nombre'
+  },3500);
+})
 changeClient(){
   this.name= 'Maria';
   this.gender='Female'
